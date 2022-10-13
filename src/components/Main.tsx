2cdfa9d91746eb3fda285ef9e0f91145d1779243
KatSchultz <ks.katieschultz@gmail.com>
2022-10-13 16:26:57 -0400

    api call working

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { giphyCall } from "../services/api.services";
 
 export default function Main() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [gif, setGif] = useState({});
+
+  useEffect(getGifs, []);
+
+  function getGifs() {
+    giphyCall().then((response) => {
+      console.log(response);
+      return response;
+    });
+  }
   return <div>Main</div>;
 }
--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -2,10 +2,12 @@ import axios from "axios";
 import { config } from "../config";
 
 export function giphyCall() {
-  return axios.get(`api.giphy.com/v1/gifs/trending`, {
-    params: {
-      api_key: config.apiKey,
-      limit: 1,
-    },
-  });
+  return axios
+    .get(`https://api.giphy.com/v1/gifs/trending`, {
+      params: {
+        api_key: config.apiKey,
+        limit: 2,
+      },
+    })
+    .then((response) => response);
 }
